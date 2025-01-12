57556c86bdb12e37ec3d37a0afbe973614e0d042
Syed Kaab Surkhi <86132908+SurkhiSyed@users.noreply.github.com>
2025-01-11 22:08:29 -0500

    connected backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
-import Home from './pages/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"; 
+import AddPatient from "./components/AddPatient"; 
+import PatientData from "./components/PatientData"; 
+import Connection from "./pages/Connection";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <div>
+        <h1>Patient Management System</h1>
+        <nav>
+          <a href="/">Home</a> | <a href="/add-patient">Add Patient</a> | <a href="/view-patient/patient123">View Patient</a>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add-patient" element={<AddPatient />} />
+          <Route path="/view-patient/:patientId" element={<PatientData />} />
+          <Route path="/updates" element={<Connection />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
